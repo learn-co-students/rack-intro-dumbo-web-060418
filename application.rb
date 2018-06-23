bf7f5f632c6cd3d4779d8a54f0516f353fc22c69
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-23 13:19:06 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Mark Sauer-Utley and I am a rack daddio."
     resp.finish
   end
 
 end
-
