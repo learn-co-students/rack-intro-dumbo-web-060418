3838248997d0c980fdf44831f8e892b95cf4ba3c
robertexe <robtabares92@gmail.com>
2018-06-25 08:20:14 -0400

    Done.

--- application.rb
+++ application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
      # All this method needs to do is return a response which consists of the status code, any headers, and the body. This can all be done using the Rack::Response object.
     resp = Rack::Response.new
-    resp.write "Hello, my name is Robert"
+    resp.write "Hello this is Robert, live from your local machine"
     resp.finish
   end
 
